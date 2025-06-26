de9eecb4c44701d02b073fed73b20c5df7870f5b
WanGyal <mac@Wangyals-MacBook-Pro.local>
2025-06-26 11:39:48 +0600

    Clean

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.get('/dashboard', userController.getDashboard);
-router.get('/foods', userController.getAllFood);
-
-module.exports = router;
